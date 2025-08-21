1fba1de9983df105751ca3ab88b6337f056eac94
anastasialin528 <anastasialin528@gmail.com>
2025-08-21 11:53:32 +0800

    Update api.js

--- api.js
+++ api.js
@@ -1,2 +1,2 @@
 // 單一真相來源：GAS Web App URL（之後改這裡就好）
-window.API_URL = 'https://script.google.com/macros/s/AKfycbyalOSHqfhnv6jG0q_MZ07cISdssvny0rqjFtyBGRlDD-wDBB8hjK9DTBahlFqB54Hc/exec';
+window.API_URL = 'https://script.google.com/macros/s/AKfycbyTlpjl4rx60TElPIFNqnm7vNwk1GiP8M885Z_SWLsMtPgx2eCFTqA8fajErx6d9g9V/exec';
