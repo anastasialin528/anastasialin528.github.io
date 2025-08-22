17e6fb06bc865c52f101ebb058ace8263edc4348
anastasialin528 <anastasialin528@gmail.com>
2025-08-22 14:42:15 +0800

    Update api.js

--- api.js
+++ api.js
@@ -1,3 +1,3 @@
 /* api.js — 全站共用的後端 Web App 端點（/exec） */
-window.API_URL = 'https://script.google.com/macros/s/AKfycbyTlpjl4rx60TElPIFNqnm7vNwk1GiP8M885Z_SWLsMtPgx2eCFTqA8fajErx6d9g9V/exec';
+window.API_URL = 'https://script.google.com/macros/s/AKfycbz-NPyTpKpt_TJOjd2mYh-x5e3nEbiD6Ae8sm8ep-D2b6lNnKTVN2TME9RBAwLX3WlF/exec';
 
